07b7a7a6a881a50d6f45d2dfe5e102b69dbf1446
Liam <github@liamjones.org>
2018-10-20 17:11:04 +0800

    Add files via upload
    Added xlrd function, now lists residents taken from excel spreadsheet.

--- ApartmentFloorCalculator.py
+++ ApartmentFloorCalculator.py
@@ -1,7 +1,11 @@
+# -*- coding: utf-8 -*-
 #Created by Liam Jones 20/09/2018
 #Designed to make it easier for me to find the floor for a particular apartment
 #in the building I work at.
-
+import xlrd
+loc = (r'C:\Users\Admin\OneDrive\Documents\Name_apartment_database.xlsx')
+wb = xlrd.open_workbook(loc)
+sheet = wb.sheet_by_index(0)
 def frontbuilding(): #Called if the apartment is below 11. The math based algorithms do not apply.
     global aptNo
     firstfloor = [8,7,3,2]
@@ -20,7 +24,8 @@ def frontbuilding(): #Called if the apartment is below 11. The math based algori
         print("East podium, the Restaurant.\n")
     elif aptNo == 6:
         print("West podium, Tan Lawyers.\n")
-    start()
+    residentname()
+
 
 #functions for each floor plan section. Because there are two sides to the building, Some floors have three apartments, some two, and there are some floor numbers not named in the building (such as 13)
 def between1144(): 
@@ -62,7 +67,7 @@ def between9497():
 def start(): #beginning. Takes input and passes to ifsandbuts function.
     global aptNo
     global compass
-    print("This program will tell you which floor the apartment is on.")
+    print("-----------------------------------------------------------\nThis program will tell you which floor the apartment is on.")
     while True:
         try:
             aptNo = int(input("What apartment number are you looking for?\n>>>"))
@@ -90,7 +95,7 @@ def ifsandbuts(): #determines which function to call based on the floor plan of 
             between5053()
         elif aptNo == 54:
             print("Apartment 54 is Floor 19, the Penthouse, West wing.\n")
-            start()
+            residentname()
         elif aptNo > 54 and aptNo < 88:
             between5587()
         elif aptNo > 87 and aptNo < 94:
@@ -99,20 +104,31 @@ def ifsandbuts(): #determines which function to call based on the floor plan of 
             between9497()
         elif aptNo == 98:
             print("Apartment 98 is Floor 19, The penthouse, East wing\n")
-            start()
+            residentname()
         else:
             print("That Number is out of range. Please try again\n")
             start()
 
 def result(): #prints out result of floor calculation along with apartment number and the wing.
+    global sheet
     global compass
     global floor
     global aptNo
     aptNo = str(aptNo)
     floor = str(floor)
     compass = str(compass)
-    print("Apartment %s is on floor %s and is in %s wing.\n\n\n" %(aptNo, floor, compass))
+    print("Apartment %s is on floor %s and is in %s wing." %(aptNo, floor, compass))
+    #sheet.cell_value(0,0)
+    #resident =(sheet.row_values(int(aptNo),0))
+    #print("Residents recorded at this address are: {} {} {}" .format(resident[1], resident[2], resident[3])) 
+    residentname()
+
+def residentname():
+    global sheet
+    global aptNo
+    sheet.cell_value(0,0)
+    resident =(sheet.row_values(int(aptNo),0))
+    print("Resident(s) recorded at this address:\n{}\n{}\n{}\n\n" .format(resident[1], resident[2], resident[3]))
     start()
 
 start()
-
