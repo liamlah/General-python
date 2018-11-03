42fece8fd771560986c1cefd0394eea4fd56a739
Liam <github@liamjones.org>
2018-11-03 21:43:23 +0800

    Add files via upload

--- ApartmentDatabaseLookup.py
+++ ApartmentDatabaseLookup.py
@@ -4,22 +4,21 @@
 import xlrd
 loc = (r'C:\Users\Admin\OneDrive\Documents\Name_apartment_database.xlsx')
 wb = xlrd.open_workbook(loc)
-
-
+print("This program allows you to search information relating to Westralian residents,\nsuch as apartment numbers, car registration or names.\n\n")
 
 
 def searchchoice(): # Menu 
     while True:
         print("-------------------------------------------------------------")
-        lookupchoice = input("What information do you already have?\n\n1)Apt number\n2)Car rego\n3)Resident name\n>>>")
+        lookupchoice = input("What information do you already have?\n\n1)Apt number\n2)Car rego\n3)Resident name\n4)Floor Number\n>>>")
         if lookupchoice == '1':            
             aptsearch()   
         elif lookupchoice == '2':
             regosearch()
         elif lookupchoice == '3':
             namesearch()
-        #elif lookupchoice == '4':
-          #  floorsearch()
+        elif lookupchoice == '4':
+            floorsearch()
         else:
             print("please make a choice between 1 and 3!\n")
 
@@ -84,13 +83,16 @@ def namesearch(): #Searches by name, - Needs to be improved for partial string m
     sheet = wb.sheet_by_index(0)
     sheet.cell_value(0,0)
     regosheet = wb.sheet_by_index(1)
-    nameinput = input("What is the name of the resident you wish to look up?\n>>>")
+    while True:
+        nameinput = input("What is the name of the resident you wish to look up?\n>>>")
+        if nameinput !="":
+            break
     nameinput = nameinput.capitalize()
     for row in range(sheet.nrows):
         for col in range(sheet.ncols):
             if nameinput == sheet.cell_value(row,col): 
                 aptNo = row
-                print(row)
+                #print(row)
     if aptNo == '':
         print("No resident with that name has been recorded.")
         searchchoice()
@@ -107,23 +109,54 @@ def namesearch(): #Searches by name, - Needs to be improved for partial string m
 
 
 #search by floor, probably not necessary
-"""
+
     
-def sidechoice():
-    sidechoice = int(input("Which side of the building are you looking for information on?\n1)East\n2)West")
+def floorsearch():
+    westfloors = { 2: '11,12,13', 3: '14,15,16', 4: '17,18,19', 5: '20,21,22', 6:'23,24,25', 7:'26,27,28.', 8:'29,30,31', 9:'32,33,34', 10:'35,36,37', 11:'38,39,40', 12:'41,42,43', 15:'44,45,46', 16:'47,48,49', 17:'50,51', 18:'52,53', 19:'54',}
+    eastfloors ={2: '55,56,57', 3: '58,59,60', 4:'61,62,63', 5:'64,65,66', 6: '67,68,69', 7: '70,71,72', 8: '73,74,75', 9: '76,77,78', 10: '79,80,81', 11: '82,83,84', 12: '85,86,87', 15: '88,89,90', 16: '91,92,93', 17: '94,95', 18:'96,97', 19:'98',}
     while True:
-        if sidechoice == 1:
-            westfloor()
+        try:
+            sidechoice = int(input("Which part of the building are you looking for information on?\n1)East\n2)West\n3)Podium\n>>>"))
             break
-        if sidechoice == 2:
-            eastfloor()
+        except ValueError:
+            print("Please make a valid selection.\n")
+
+    while True:
+        if sidechoice <1 or sidechoice >3:
+            print("Please select from East, West or Podium.")
+            floorsearch()
+        elif sidechoice == 3:
+            print("East podium has recipe #1 on Ground floor, #2 and #3 on floor 1, with #4 and #5 on floor 2.\nWest podium has Tan Lawyers #6 on Ground floor, #7 and #8 on floor 1, with #9 and #10 on floor 2.\n")
             break
+        else:
+            break
+    while True:
+        try:
+            sidefloor = int(input("Which floor are you looking for? (2-19)\n>>>"))
+            if sidechoice == 2:  
+                print("Apartments on floor {} of the west building are {}.\n ".format (sidefloor, westfloors[sidefloor]))
+                break
+            elif sidechoice == 1:
+                print("Apartments on floor {} of the East building are {}.\n ".format (sidefloor, eastfloors[sidefloor]))
+                break
+        except KeyError:
+            print("Such Floor does not exist.\n")
+        except ValueError:        
+            print("Such floor does not exist.\n")
 
-def eastfloor():
-    floorchoice = int(input("What Floor are you looking for information on in the west side?
+    while True:
+        doapartment = input("Do you want to do a further search on one of these apartments?\n1)Yes\n2)No\n>>>")
+        if doapartment == '1':
+            aptsearch()
+            break
+        if doapartment == '2':
+            searchchoice()
+            break
+        else:
+            print("Make a valid selection, 1 or 2.")
+            
+                       
 
 
-def westfloor():                     
-"""
 
 searchchoice()
