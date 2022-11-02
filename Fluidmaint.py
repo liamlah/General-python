f31d5d80528bddc0a1a8e787ee916049df1d3eec
Liam <github@liamjones.org>
2022-11-02 16:37:44 +0800

    Added second mode and a menu

--- Fluidmaint.py
+++ Fluidmaint.py
@@ -1,13 +1,23 @@
 import random
-"""
-while True:        
+
+def menu():
+    print("Online fluid replacement and maintenance trainer.\n")
+    while True:        
         try:
-            questionbox = int(input("What is the %s maintenance rate in ml/hr for a %skg patient?\n$"%(ptweight)))
+            menuchoice = int(input("Choose your difficulty level:\nPress 1 for fluid maintenance practice only (basic4:2:1)\nPress 2 for maintenance and replacement practice\n\n#"))
             break        
         except ValueError:
-            print("put an integer and try again") 
-"""
-"""
+            print("please choose a number")
+    if menuchoice == 1:
+        print('You have chosen maintenance practice, enter 0 at any time to return to the menu.\n')
+        fluidmaint()
+    elif menuchoice == 2:
+        print('You have chosen maintenance and replacement practice, enter 0 at any time to return to the menu.\n')
+        fluidmaintreplace()
+    else:
+        print("please select 1 or 2")
+         
+
 def fluidmaint():
     ptweight = random.randint(1,50) #adjust the second number if you want a different weight limit
     if ptweight >19:
@@ -27,16 +37,23 @@ def fluidmaint():
     if questionbox == maintrate:
         print("CORRECT, the maintenance rate is: %sml/hr\n"%(maintrate))
         fluidmaint()
+    elif questionbox == 0:
+        menu()
     else:
         print("WRONG the maintenance rate is: %sml/hr\n"%(maintrate))
         fluidmaint()
-"""
+
 def fluidmaintreplace():
     ptweight = random.randint(1,50) #adjust the second number if you want a different weight limit
     ptdehydration = random.randint(1,12)
-    rehydrationtime = [4,24,48]
-    hrday = ["hourly","daily"]
+    hrday = ["*hourly*","*daily*"]
     hoursordays = hrday[random.randint(0,1)]
+    if hoursordays =='*daily*':
+        rehydrationoptions =[24,48]
+        rehydrotime = rehydrationoptions[random.randint(0,1)]
+    else:
+        rehydrationoptions = [4,24,48]
+        rehydrotime = rehydrationoptions[random.randint(0,2)]
     if ptweight >19:
         extraweight = ptweight - 20
         maintrate =  extraweight + 60
@@ -45,32 +62,33 @@ def fluidmaintreplace():
         maintrate = extraweight * 2 + 40
     else:
         maintrate = ptweight * 4
+    totalreplace = ptweight *(ptdehydration*10) 
+    hourlyreplace = totalreplace/rehydrotime
+    if hoursordays == '*hourly*':
+        finalanswer = hourlyreplace + maintrate
+        finalanswer = int(finalanswer)
+        rate ="hour"
+    elif hoursordays == '*daily*':
+        finalanswer = (hourlyreplace*24) + (maintrate*24)
+        finalanswer = int(finalanswer)
+        rate ="day"
     while True:        
         try:
-            questionbox = int(input("What is the %s maintenance rate in ml/hr for a %skg patient?\n$"%(ptweight)))
+            questionbox = int(input("What is the {} combined maintenance and replacement rate in ml for a {} percent dehydrated patient who weighs {}kg to be rehydrated over {} hours?\n$" .format(hoursordays, ptdehydration, ptweight, rehydrotime)))
             break        
         except ValueError:
             print("put an integer and try again") 
-    if questionbox == maintrate:
-        print("CORRECT, the maintenance rate is: %sml/hr\n"%(maintrate))
+    if int(finalanswer) <= questionbox <= 1+int(finalanswer):
+        print("CORRECT, the fluid amount is: {}ml/{}\nworking: the hourly rate is {}ml/hr and maintenance rate is {}ml/hr\n".format(finalanswer,rate,int(hourlyreplace),int(maintrate)))
         fluidmaintreplace()
+    elif questionbox == 0:
+        menu()
     else:
-        print("WRONG the maintenance rate is: %sml/hr\n"%(maintrate))
+        print("WRONG the fluid amount is: {}ml/{}\nworking: the hourly rate is {}ml/hr and maintenance rate is {}ml/hr\n".format(finalanswer,rate,int(hourlyreplace),int(maintrate)))
         fluidmaintreplace()
 
 
-fluidmaintreplace()
+menu()
 
 
-"""slow replacement = 48h e.g 5% + maintenance
- IV slow 
 
-# septic shock
-20ml/kg bolus. ]
-
-
-Hard mode
-dehydration level
-rehydration period
-+ daily maintenance
-"""
